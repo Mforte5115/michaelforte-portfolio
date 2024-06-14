d5689fc4e8711150cb72e1261f6c760835ed2ee6
mforte5115 <mforte@udel.edu>
2024-06-14 18:23:03 -0400

    Added technology page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { HomePage } from './Components/HomePage/HomePage';
 import { AboutMe } from './Components/AboutMe/AboutMe';
 import { NavBar } from "./Components/NavBar/NavBar"
 import {ParticlesEffect} from './Components/Particles/Particles'
+import { Technologies } from './Components/Technologies/Technologies'
 
 function App() {
   const [page, setPage] = useState<string>("Home");
@@ -24,6 +25,9 @@ function App() {
         </div>
         <div className="snap-always snap-center px-0" id="aboutme">
           <AboutMe />
+        </div>
+        <div className="snap-always snap-center" id="technologies">
+          <Technologies />
         </div>
       </div>
     </div>
---
+++ src/Components/Technologies/Technologies.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export function Technologies() {
+
+    return (
+        <div className={'relative h-dvh flex'}>
+            <div className="container max-w-screen-xl mx-auto flex justify-center items-center space-x-8">
+                Hello!
+            </div>
+        </div>
+    )
+}
