b3772e30b33fb3760bc117f9640bf21cee2cd97f
mforte5115 <mforte@udel.edu>
2024-06-20 20:53:30 -0400

    Created timeline

--- src/Components/Experience/Experience.tsx
+++ src/Components/Experience/Experience.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Timeline } from '../Timeline/Timeline'
 
 export function Experience() {
 
     return (
         <div className={'relative h-dvh flex px-0'}>
             <div style={{fontFamily: 'CustomPixelFont'}} className="container max-w-screen-xl mx-auto flex flex-col justify-start md:justify-center items-center text-2xl space-y-8">
-                Hello!
+                <Timeline/>
             </div>
         </div>
     )
--- src/Components/Timeline/Timeline.tsx
+++ src/Components/Timeline/Timeline.tsx
@@ -4,14 +4,28 @@ import { Chrono } from 'react-chrono'
 
 const items = [
     {
-        title: 
+        title: "Summer 2022",
+        cardTitle: "PSEG Long Island",
+        cardSubtitle: "Project Management Intern",
+        cardDetailedText: "During the summer of 2022 I worked as a project management intern. I helped manage a project to replace 1000 power transmission lines, managing timelines and surveys myself."
+    },
+    {
+        title: "Summer 2023",
+        cardTitle: "Mack Trucks",
+        cardSubtitle: "Manufacturing Engineering Intern",
+        cardDetailedText: "The summer of 2023 I worked as a manufacturing engineering intern. I reverse engineered and designed a custom wiring harness for diagnosing truck testing issues. Executed redesign of multiple factory floor stations."
     }
 ]
 
 
 export function Timeline() {
     return (
-        <Chrono
+        <Chrono 
+            items={items}
+            mode="HORIZONTAL"
+            itemWidth={150}
+            showSingle 
+            />
 
     )
 }
